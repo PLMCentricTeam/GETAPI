05a8dce7288877a340fc37af2213b8a216137331
PLMCentricTeam <165859888+PLMCentricTeam@users.noreply.github.com>
2024-06-24 11:57:44 +0700

    Add files via upload

---
+++ Master Data/MasterStylingInfo.js
@@ -0,0 +1,11 @@
+const MasterStylingInfo = [
+    'AHF',
+    'BIG',
+    'BNT',
+    'IRN',
+    'NIR',
+    'REG',
+    'SNI',
+    'TFT',
+    'TLL',
+];
---
+++ Master Data/MasterWashSubCategory.js
@@ -0,0 +1,10 @@
+const MasterWashSubCategory = [
+    'DP1',
+    'DP2',
+    'DP3',
+    'DP4',
+    'DYE',
+    'DYP',
+    'NDP',
+    'NWP',
+];
